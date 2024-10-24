1881ead8cdd015122b73f90263e569082fd527a9
vondarkness <aroli.marcellinus@gmail.com>
2024-10-24 18:16:28 +0900

    Adding template for bash script executable

---
+++ bin/simulation_dirs_bash/drugsim_ord_cvode/exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Use this to export the library path
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/prog/sundials/sundials-5.7.0/lib:/usr/local/lib64:/usr/lib64
+echo $LD_LIBRARY_PATH
+export PATH=$PATH
+echo $PATH
+
+
+#find . -name "*.plt" -type f -delete
+rm -rf *.log result log
+mkdir result
+mpirun -np 24 ~/marcell/MetaHeart/DrugSimulation/bin/drugsim_ord -input_deck param.txt > logfile
