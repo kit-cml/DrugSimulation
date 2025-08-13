03b83ecb18df7559d00069d574cae77b67b67e59
vondarkness <aroli.marcellinus@gmail.com>
2025-08-13 11:47:47 +0900

    Adding the population simulation directory

---
+++ bin/simulation_cpu_population/exec_bash.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Source the function script
+source "../scripts/create_concs_directories.sh"
+source "../scripts/zip_files.sh"
+
+# Use this to export the library path.
+# Please change the directory according to your library's location.
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}":/opt/prog/sundials/sundials-5.7.0/lib:/usr/local/lib64:/usr/lib64
+echo "${LD_LIBRARY_PATH}"
+export PATH="${PATH}"
+echo "${PATH}"
+
+# Logging starts from here
+RESULT_FOLDER="./results"
+echo "DrugSimulation Log Start..." >& "${RESULT_FOLDER}/logfile"
+
+# Clear any old PID file
+PIDFILE="mpiexec.pid"
+rm -f "${PIDFILE}"
+
+
+NUMBER_OF_CPU="$(grep "^number_of_cpu" param.txt | cut -d'=' -f2 | sed 's/\/\/.*//' | xargs)"
+MAX_CPU="$(nproc --all)"
+if [[ "${NUMBER_OF_CPU}" -gt  "${MAX_CPU}" ]]; then
+  echo "The processor requested exceed the number of maximum of CPU in this PC!" >> "${RESULT_FOLDER}/logfile" 2>&1
+  exit 1
+fi
+
+# to grab cell_model value from parameter file (thanks, ChatGPT).
+# grep "^user_name": looks for the line starting with user_name
+# cut -d'=' -f2: gets the right-hand side of =
+# sed 's/\/\/.*//': removes any inline comment starting with //
+# xargs: trims leading and trailing whitespace
+CELL_MODEL="$(grep "^cell_model" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)"
+USER_NAME="$(grep "^user_name" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)"
+DRUG_NAME="$(grep "^drug_name" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)"
+DRUG_CONCENTRATIONS="$(grep "^drug_concentrations" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)"
+
+INITIAL_VALUES_SUBSTRING="*initial_values*.csv"
+INITIAL_VALUES_ZIPNAME="${DRUG_NAME}_${CELL_MODEL}_initial_values.zip"
+TIME_SERIES_SUBSTRING="*time_series*.csv"
+TIME_SERIES_ZIPNAME="${DRUG_NAME}_${CELL_MODEL}_time_series.zip"
+FEATURES_SUBSTRING="*features*.csv"
+FEATURES_ZIPNAME="${DRUG_NAME}_${CELL_MODEL}_features.zip"
+LAST_PACES_SUBSTRING="*last_paces*.csv"
+LAST_PACES_ZIPNAME="${DRUG_NAME}_${CELL_MODEL}_last_paces.zip"
+
+# Split the string into an array
+IFS=',' read -r -a drug_concentrations <<< "${DRUG_CONCENTRATIONS}"
+
+# Logging starts from here
+RESULT_FOLDER="./results"
+echo "Cleaning ${RESULT_FOLDER}"
+rm -rf "${RESULT_FOLDER}"
+echo "Cleaning successful!"
+create_drug_concentration_directories "${RESULT_FOLDER}" "${DRUG_NAME}" "${CELL_MODEL}" "${drug_concentrations[@]}"
+echo "DrugSimulationPostprocessing Log Start..." >& "${RESULT_FOLDER}/logfile"
+
+# Clear any old PID file
+PIDFILE="mpiexec.pid"
+rm -f "${PIDFILE}"
+
+
+# choose the binary based on the value of cell_model
+if [[ "${CELL_MODEL}" == *"CiPAORdv1.0"* ]]; then
+  BINARY_FILE=../drugsim_CiPAORdv1.0
+elif [[ "${CELL_MODEL}" == *"ORd-static"* ]]; then
+  BINARY_FILE=../drugsim_ORd-static
+elif [[ "${CELL_MODEL}" == *"ToR-ORd"* ]]; then
+  BINARY_FILE=../drugsim_ToR-ORd
+elif [[ "${CELL_MODEL}" == *"ToR-ORd-dynCl"* ]]; then
+  BINARY_FILE=../drugsim_ToR-ORd-dynCl
+else
+  echo "The cell model ${CELL_MODEL} is not specified to any simulations!!" >> "${RESULT_FOLDER}/logfile" 2>&1
+  exit 1
+fi
+
+echo "Run $CELL_MODEL cell model simulation with ${NUMBER_OF_CPU} cores."
+( echo $$ > "${PIDFILE}"; exec mpiexec -np "${NUMBER_OF_CPU}" "${BINARY_FILE}" -input_deck param.txt >> "${RESULT_FOLDER}/logfile" 2>&1 )
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Simulation program got some problems!!! Exiting..."
+  exit 1
+fi
+echo "Zipping folder..." >> "${RESULT_FOLDER}/logfile" 2>&1
+zip_files "${RESULT_FOLDER}" "${INITIAL_VALUES_SUBSTRING}" "${INITIAL_VALUES_ZIPNAME}"
+zip_files "${RESULT_FOLDER}" "${TIME_SERIES_SUBSTRING}" "${TIME_SERIES_ZIPNAME}"
+zip_files "${RESULT_FOLDER}" "${FEATURES_SUBSTRING}" "${FEATURES_ZIPNAME}"
+zip_files "${RESULT_FOLDER}" "${LAST_PACES_SUBSTRING}" "${LAST_PACES_ZIPNAME}"
+mv "${INITIAL_VALUES_ZIPNAME}" "${RESULT_FOLDER}/."
+mv "${TIME_SERIES_ZIPNAME}" "${RESULT_FOLDER}/."
+mv "${FEATURES_ZIPNAME}" "${RESULT_FOLDER}/."
+mv "${LAST_PACES_ZIPNAME}" "${RESULT_FOLDER}/."
+echo "Zipping finished" >> "${RESULT_FOLDER}/logfile" 2>&1
+sh "./generate_report.sh" >> "${RESULT_FOLDER}/logfile" 2>&1
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "Reporting program got some problems!!! Exiting..." >> "${RESULT_FOLDER}/logfile" 2>&1
+  exit 1
+fi
+echo "Simulation has finished! Check the logfile for more details." >> "${RESULT_FOLDER}/logfile" 2>&1
---
+++ bin/simulation_cpu_population/generate_report.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+RESULT_FOLDER="./results"
+DRUG_NAME=$(grep "^drug_name" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)
+USER_NAME=$(grep "^user_name" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)
+DRUG_CONCENTRATIONS=$(grep "^drug_concentrations" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)
+CELL_MODEL=$(grep "^cell_model" param.txt | cut -d'=' -f2 | cut -d'/' -f1 | cut -d'/' -f1 | sed 's/\/\/.*//' | xargs)
+
+HILL_FILE=$(grep "^hill_file" param.txt | cut -d '=' -f2 | sed 's|//.*||' | xargs)
+echo "FILE HILL: ${HILL_FILE}"
+SAMPLE_SIZE=$(( $(wc -l < "${HILL_FILE}") - 1 ))
+echo "FILE LINE: ${SAMPLE_SIZE}"
+
+RESULT_DRUG_PATH=${RESULT_FOLDER}
+LATEX_FILE=report_drug_${DRUG_NAME}_${CELL_MODEL}.tex
+
+echo "TESTING RESULT: ${RESULT_DRUG_PATH}"
+#Plot all the time-series result from the in-silico simulation
+echo Generate plots from the time-series result
+python3 ../scripts/plot_time_series.py $RESULT_DRUG_PATH $DRUG_NAME $SAMPLE_SIZE
+PROGRESS=$((PROGRESS + 25)) # 25% for plot generating.
+echo "DrugSimulation Report Progress: $PROGRESS%"
+
+#Concat the separated feature data
+echo Unifying feature data
+python3 ../scripts/plot_features.py $RESULT_DRUG_PATH $DRUG_NAME $USER_NAME
+PROGRESS=$((PROGRESS + 25)) # 25% for plot generating.
+echo "DrugSimulation Report Progress: $PROGRESS%"
+
+#Generate report based on the pre-generated LaTEX file
+echo "Generate PDF from LaTEX"
+cd "${RESULT_FOLDER}"
+pdflatex $LATEX_FILE
+PROGRESS=$((PROGRESS + 50)) # 25% for plot generating.
+echo "DrugSimulation Report Progress: $PROGRESS%"
